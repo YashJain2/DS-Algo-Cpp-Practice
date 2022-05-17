4913ba88afea1490ad1ac90757e59d2fccfdd562
Yash <yjain8958372013@gmail.com>
2022-05-17 19:52:46 +0530

    Hashing

---
+++ apni kaksha c++ codes/31. Hashing/Hashing-Map.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //stores key value pair
+    map<int,int> mp;
+    //here 6 is the key to the value 1 
+    mp[6] = 1;
+    cout<< mp[6] << endl;
+    return 0;
+}
